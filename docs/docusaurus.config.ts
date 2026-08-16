651f062c143912ed6aadb41369b5ce207b14c5b4
denis <denis.kibalko@nimbusweb.me>
2026-08-16 15:23:49 +0300

    fix edit docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/fusebase-dev/codee/tree/main/docs/',
+            'https://github.com/fusebase-dev/codee/tree/master/docs/',
         },
         theme: {
           customCss: './src/css/custom.css',
